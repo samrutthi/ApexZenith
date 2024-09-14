f3465f41f696eeaeb4187e6ab1026d56da6a93c3
samrutthi <samrutthisaravanan234@gmail.com>
2024-09-14 07:45:30 +0530

    Add files via upload

---
+++ final.js
@@ -0,0 +1,11 @@
+// Function to set the role in the modal dynamically
+function setRole(role) {
+    document.getElementById('roleName').innerText = role;
+  }
+  
+  // Optional: If you need to scroll to a specific section, this function can be useful
+  function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  
